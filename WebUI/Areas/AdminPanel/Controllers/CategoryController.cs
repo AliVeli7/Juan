b6e6175256e50a4007721cd2b8631e182dd2eca7
AliVeli7 <6gmrj9e@code.edu.az>
2022-06-11 23:41:41 +0400

    23:41

--- WebUI/Areas/AdminPanel/Controllers/CategoryController.cs
+++ WebUI/Areas/AdminPanel/Controllers/CategoryController.cs
@@ -9,6 +9,7 @@ using WebUI.ViewModels.Categoryy;
 
 namespace WebUI.Areas.AdminPanel.Controllers
 {
+    [Area("AdminPanel")]
     public class CategoryController : Controller
     {
         private AppDbContext _context { get; }
@@ -18,7 +19,7 @@ namespace WebUI.Areas.AdminPanel.Controllers
             _context = context;
             categories = _context.Categories.Where(ct => !ct.IsDeleted);
         }
-        [Area("AdminPanel")]
+       
         public IActionResult Index()
         {
             return View(categories);
@@ -56,6 +57,17 @@ namespace WebUI.Areas.AdminPanel.Controllers
             return RedirectToAction(nameof(Index));
         }
 
-       
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+                return BadRequest();
+            Categories categoryDb = _context.Categories.Where(c => c.IsDeleted).FirstOrDefault(c => c.Id == id);
+            if (categoryDb == null)
+                return NotFound();
+            // _context.Categories.Remove(categoryDb);
+            categoryDb.IsDeleted = true;
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
     }
 }
