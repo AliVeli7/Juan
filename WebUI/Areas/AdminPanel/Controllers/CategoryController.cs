bec429a249b70895784e45794c941d0474c036ea
AliVeli7 <6gmrj9e@code.edu.az>
2022-06-11 19:58:40 +0400

    19:58

--- WebUI/Areas/AdminPanel/Controllers/CategoryController.cs
+++ WebUI/Areas/AdminPanel/Controllers/CategoryController.cs
@@ -3,11 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using WebUI.DAL;
+using WebUI.Models;
 
 namespace WebUI.Areas.AdminPanel.Controllers
 {
     public class CategoryController : Controller
     {
+        private AppDbContext _context { get; }
+        private IEnumerable<Categories> categories;
+        public CategoryController(AppDbContext context)
+        {
+            _context = context;
+            categories = _context.Categories.Where(ct => !ct.IsDeleted);
+        }
         [Area("AdminPanel")]
         public IActionResult Index()
         {
