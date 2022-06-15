0f9f6e2ed4b3008042d39a205b1fed12f9b2e3c7
AliVeli7 <6gmrj9e@code.edu.az>
2022-06-15 12:46:09 +0400

    product crud

--- WebUI/Areas/AdminPanel/Controllers/ProductController.cs
+++ WebUI/Areas/AdminPanel/Controllers/ProductController.cs
@@ -81,5 +81,91 @@ namespace WebUI.Areas.AdminPanel.Controllers
             await _context.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
         }
+
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+                return BadRequest();
+            Products productDb = _context.Products.Where(c => !c.isDeleted).FirstOrDefault(c => c.Id == id);
+            if (productDb == null)
+                return NotFound();
+            // _context.Products.Remove(ProductDb);
+            productDb.isDeleted = true;
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Update(int? id)
+        {
+            if (id == null)
+            {
+                return BadRequest();
+            }
+            var product = _context.Products.Find(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            ViewBag.colors = _context.Colors.ToList();
+            return View(product);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Update(int? id, Products newProduct, int colorId)
+        {
+            if (id == null)
+            {
+
+            }
+            var ProductDb = _context.Products.Find(id);
+            if (ProductDb == null)
+            {
+                return NotFound();
+            }
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+
+            if (!newProduct.Photo.CheckFileSize(200))
+            {
+                ModelState.AddModelError("Photo", "Image size must be smaller than 200kb");
+                return View();
+            }
+            if (!newProduct.Photo.CheckFileType("image/"))
+            {
+                ModelState.AddModelError("Photo", "Type of file  must be image");
+                return View();
+            }
+            Products UpdatedProduct = new Products
+            {
+                Name = newProduct.Name,
+                Price = newProduct.Price,
+
+            };
+            string image = await newProduct.Photo.SaveFileAsync(_env.WebRootPath, "assets/img/product");
+            ProductColors productcolor = new ProductColors()
+            {
+                ColorId = colorId
+            };
+
+            ProductImages productImages = new ProductImages
+            {
+                Url = image
+            };
+            List<ProductImages> images = new List<ProductImages>();
+            List<ProductColors> colors = new List<ProductColors>();
+            images.Add(productImages);
+            colors.Add(productcolor);
+            UpdatedProduct.Images = images;
+            UpdatedProduct.Colors = colors;
+            Products productDb = _context.Products.Where(c => !c.isDeleted).FirstOrDefault(c => c.Id == id);
+            productDb.isDeleted = true;
+            await _context.Products.AddAsync(UpdatedProduct);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+
+        }
+
     }
 }
